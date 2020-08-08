be42e30fdcd7ec549ff1e481e349599dd63b3924
DarioGiacomelli <dario@encomo.be>
2020-08-08 23:22:49 +0200

    WiP

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+import os
+import subprocess
 
-
-
+#Show menu
 def menu():
     while True:
         print("*" * 30)
@@ -20,16 +21,46 @@ def menu():
         else:
             choice(menuChoice)
             
-
+# MenuItems
 def choice(menuChoice):
     if menuChoice == 1:
         print("Starting HTB.vpn")
         HTBVpn()
+    elif menuChoice == 2:
+        print("Making HTB folder")
+        machine = raw_input("enter machinename: ")
+        ip = raw_input("enter IP address: " )
+        mkdirHTB(machine, ip)
+    elif menuChoice == 3:
+        print("Run Nmap")
+        nmap()
     else:
-        print("You did not choose '1'")
-        
+        print("You did not choose a valid option")
 
+# Functions
 def HTBVpn():
     print("This is where I would put my code... If I had any")
 
+def mkdirHTB(machine, ip):
+    path = os.path.expanduser("~/HTB/Machines/")
+    folder = path + machine + "/"
+
+    try:
+        os.mkdir(folder)
+    except  OSError:
+        print("Creation failed")
+    else:
+        print("created folder " + folder)
+        print("making Nmap folder")
+        nmapfolder = folder + "nmap/"
+
+        try:
+            os.mkdir(nmapfolder)
+        except OSError:
+            print("Creation failed")
+        else:
+            print("created folder" + nmapfolder)
+            wait = raw_input("Press any key to return to main menu\n")
+
+#Call MainMenu
 menu()
