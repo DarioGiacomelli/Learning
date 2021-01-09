2d2075824140b0b80c03fb955f3e6e1d3b9a5186
DarioGiacomelli <dario@encomo.be>
2021-01-09 21:05:49 +0100

    .

--- Time/test.py
+++ Time/test.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python
 import PySimpleGUI as sg
 
-"""
-    Simple test harness to demonstate how to use the CalendarButton and the get date popup
-"""
-# sg.theme('Dark Red')
-layout = [[sg.Text('Date Chooser Test Harness', key='-TXT-')],
+layout = [[sg.Text('Tijdsregistratie', key='-TXT-')],
       [sg.Text(key='-IN-', size=(20,1))],
       [sg.CalendarButton('Datum',  target='-IN-', locale='nl_NL', begin_at_sunday_plus=1, format='%d/%m/%Y' )]]
 
